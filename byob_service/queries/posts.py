7a9ca630c0bd4b0a75d618795e6ce91cb9e41f71
Edward Shinwoo Sohn <edwardsohn96@gmail.com>
2023-03-30 22:27:49 +0000

    added users queries and pool #15

---
+++ byob_service/queries/posts.py
@@ -0,0 +1,4 @@
+from pydantic import BaseModel
+from typing import Optional, List, Union
+from datetime import date
+from queries.pool import pool
---
+++ byob_service/queries/users.py
@@ -0,0 +1,39 @@
+from pydantic import BaseModel
+from typing import Optional, List, Union
+from datetime import date
+from queries.pool import pool
+
+
+class UsersIn(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    address: str
+    city: str
+    state: str
+    username: str
+    password: str
+    avatar_url: Optional[str]
+
+
+class UsersOut(BaseModel):
+    user_id: int
+    first_name: str
+    last_name: str
+    email: str
+    phone_number: str
+    address: str
+    city: str
+    state: str
+    username: str
+    password: str
+    avatar_url: Optional[str]
+    is_driver: bool
+    car_model: Optional[str]
+    license_plate: Optional[str]
+    dl_number: Optional[str]
+
+
+class UsersOutWithPassword(UsersOut):
+    hashed_password: str
