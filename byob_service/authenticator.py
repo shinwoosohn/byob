c863c66fba9a7e449efbb3eebe1d5d3706aedac5
Garrett Hoffman <son0hoff@gmail.com>
2023-03-30 17:57:07 -0700

    Added authenticator file following jwtdown-fastapi docs and created random genearted 21 digit signing key for the BYOB service to utilize the authenticator. Updated Docker Compose to include signing key in environment variables under byob_service.

---
+++ byob_service/authenticator.py
@@ -0,0 +1,36 @@
+# authenticator.py
+import os
+from fastapi import Depends
+from jwtdown_fastapi.authentication import Authenticator
+from queries.users import UsersRepository, UsersOut, UsersIn
+
+
+class MyAuthenticator(Authenticator):
+    async def get_account_data(
+        self,
+        username: str,
+        users: UsersRepository,
+    ):
+        # Use your repo to get the user based on the
+        # username (which could be an email)
+        return users.get(username)
+
+    def get_user_getter(
+        self,
+        users: UsersRepository = Depends(),
+    ):
+        # Return the users. That's it.
+        return users
+
+    def get_hashed_password(self, user: UsersIn):
+        # Return the encrypted password value from your
+        # user object
+        return user.hashed_password
+
+    def get_user_data_for_cookie(self, user: UsersIn):
+        # Return the username and the data for the cookie.
+        # You must return TWO values from this method.
+        return user.username, UsersOut(**user.dict())
+
+
+authenticator = MyAuthenticator(os.environ["SIGNING_KEY"])
