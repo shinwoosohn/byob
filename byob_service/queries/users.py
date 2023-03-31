cb0b310ab44ae4f5e7e6490ca1a9df5dffc8c415
Garrett Hoffman <son0hoff@gmail.com>
2023-03-30 18:55:08 -0700

    Updating for user creation api endpoint using jwtdown-fastapi library from documentation. Added some generic import statements to the various routers as a small unimportant skeleton update. Updated Requirements for FASTAPI version from .78 to .92 and included jwtdown-fastapi .5. Updated User migration table for 'unique' label on email, phone number, and username.

--- byob_service/queries/users.py
+++ byob_service/queries/users.py
@@ -40,4 +40,60 @@ class UsersOutWithPassword(UsersOut):
 
 
 class UsersRepository:
-    pass
+
+    # Create method for Users
+    def create(self, users: UsersIn,
+               hashed_password: str) -> UsersOutWithPassword:
+        try:
+            # connect the database
+            with pool.connection() as conn:  # with keyword is called a monitor and a way to not try catch block
+                # get a cursor (something to run SQL with)
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                        # Insert statement part of SQL execute method
+                        # using value syntax '%...'
+                        """
+                        INSERT INTO users
+                            (
+                                first_name,
+                                last_name,
+                                email,
+                                phone_number,
+                                address,
+                                city,
+                                state,
+                                username,
+                                hashed_password,
+                                avatar_url
+                            )
+                        VALUES
+                            (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        RETURNING id AS user_id;
+                        """,
+                        [
+                            users.first_name,
+                            users.last_name,
+                            users.email,
+                            users.phone_number,
+                            users.address,
+                            users.city,
+                            users.state,
+                            users.username,
+                            users.hashed_password,
+                            users.avatar_url
+                        ]
+                    )
+                    user_id = result.fetchOne()[0]
+                    old_data = users.dict()
+                    return UsersOutWithPassword(
+                        user_id = user_id,
+                        **old_data,
+                        hashed_password=hashed_password
+                    )
+        except Exception as e:
+            if 'username' in str(e):
+                raise ValueError('Username already exists')
+            elif 'email' in str(e):
+                raise ValueError('Email already exists')
+            elif 'phone_number' in str(e):
+                raise ValueError('Phone number already exists')
