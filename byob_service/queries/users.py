ac931a4ee93eee158314149af3dbec19560609d6
Edward Sohn <edwardsohn96@gmail.com>
2023-04-03 14:14:24 -0700

    added get user method in router and queries for API endpoint. added encoder for fetchone

--- byob_service/queries/users.py
+++ byob_service/queries/users.py
@@ -39,10 +39,10 @@ class UsersOutWithPassword(UsersOut):
 
 
 class UsersRepo:
-
     # Create method for Users
-    def create(self, users: UsersIn,
-               hashed_password: str) -> UsersOutWithPassword:
+    def create(
+        self, users: UsersIn, hashed_password: str
+    ) -> UsersOutWithPassword:
         try:
             # connect the database
             with pool.connection() as conn:  # with keyword is called a monitor and a way to not try catch block
@@ -79,20 +79,74 @@ class UsersRepo:
                             users.state,
                             users.username,
                             users.hashed_password,
-                            users.avatar_url
-                        ]
+                            users.avatar_url,
+                        ],
                     )
                     user_id = result.fetchOne()[0]
                     old_data = users.dict()
                     return UsersOutWithPassword(
-                        user_id = user_id,
+                        user_id=user_id,
                         **old_data,
                         hashed_password=hashed_password
                     )
         except Exception as e:
-            if 'username' in str(e):
-                raise ValueError('Username already exists')
-            elif 'email' in str(e):
-                raise ValueError('Email already exists')
-            elif 'phone_number' in str(e):
-                raise ValueError('Phone number already exists')
+            if "username" in str(e):
+                raise ValueError("Username already exists")
+            elif "email" in str(e):
+                raise ValueError("Email already exists")
+            elif "phone_number" in str(e):
+                raise ValueError("Phone number already exists")
+
+    # Get a specific user method for Users
+    def get_user(self, user_id: int) -> Optional[UsersOutWithPassword]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as cur:
+                    result = cur.execute(
+                        """
+                        SELECT id AS user_id
+                            , first_name
+                            , last_name
+                            , email
+                            , phone_number
+                            , address
+                            , city
+                            , state
+                            , username
+                            , hashed_password
+                            , avatar_url
+                            , is_driver
+                            , car_model
+                            , license_plate
+                            , dl_number
+                        FROM users
+                        WHERE id = %s
+                        """,
+                        [user_id],
+                    )
+                    record = result.fetchone()
+                    if record is None:
+                        return None
+                    return self.record_to_user_out(record)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not get that user"}
+
+    def record_to_user_out(self, record):
+        return UsersOutWithPassword(
+            user_id=record[0],
+            first_name=record[1],
+            last_name=record[2],
+            email=record[3],
+            phone_number=record[4],
+            address=record[5],
+            city=record[6],
+            state=record[7],
+            username=record[8],
+            hashed_password=record[9],
+            avatar_url=record[10],
+            is_driver=record[11],
+            car_model=record[12],
+            license_plate=record[13],
+            dl_number=record[14],
+        )
