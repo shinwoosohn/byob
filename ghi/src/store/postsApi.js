e98669c067a58031d3d9ab478cc44e95864d1a49
Edward Shinwoo Sohn <edwardsohn96@gmail.com>
2023-04-06 23:10:00 +0000

    added PUT endpoint for posts/listings

---
+++ ghi/src/store/postsApi.js
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const postsApi = createApi({
+  reducerPath: "posts",
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.REACT_APP_BYOB_SERVICE_API_HOST,
+  }),
+  endpoints: (builder) => ({
+    getAllPosts: builder.query({
+      query: () => "/posts",
+      providesTags: ["postsList"],
+    }),
+    getPosts: builder.query({
+      query: () => "/posts/" + id,
+    }),
+    createPosts: builder.mutation({
+      query: (data) => ({
+        url: "/posts",
+        body: data,
+        method: "post",
+      }),
+      invalidatesTag: ["postsList"],
+    }),
+    updatePosts: builder.mutation({
+      query: (id, data) => ({
+        url: "/posts/" + id,
+        body: data,
+        method: "put",
+      }),
+      invalidatesTags: ["postsList"],
+    }),
+    deletePosts: builder.mutation({
+      query: (id) => ({
+        url: "/posts/" + id,
+        method: "delete",
+      }),
+      invalidatesTags: ["postsList"],
+    }),
+  }),
+});
+
+export const {
+  useGetPostsQuery,
+  useCreatePostsMutation,
+  useGetAllPostsQuery,
+  useDeletePostsMutation,
+  useUpdatePostsMutation,
+} = postsApi;
