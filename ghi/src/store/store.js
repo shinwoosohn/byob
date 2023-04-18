6434d9e0ba2b0e7592e16271ca8bb113e50cd8c4
Timothy Thompson <tjrthompson@gmail.com>
2023-04-18 19:23:55 +0000

    Resolve "redux-store-user-api"

--- ghi/src/store/store.js
+++ ghi/src/store/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { setupListeners } from "@reduxjs/toolkit/query";
 // import { produceApi } from "./produceApi";
+import { usersApi } from "./usersApi";
 import { postsApi } from "./postsApi";
 import { authApi } from "./authApi";
 import { userSlice } from "./user";
@@ -10,11 +11,13 @@ export const store = configureStore({
     auth: userSlice.reducer,
     [authApi.reducerPath]: authApi.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(authApi.middleware)
-      .concat(postsApi.middleware),
+      .concat(postsApi.middleware)
+      .concat(usersApi.middleware),
 });
 
 setupListeners(store.dispatch);
