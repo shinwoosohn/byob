27a32b9d3129435c77c70fdf9132d0ba34b2435e
Timi <tjrthompson@gmail.com>
2023-04-18 12:57:59 -0700

    psuedo code outline for user detail page up

--- ghi/src/User/ProfileDetail.js
+++ ghi/src/User/ProfileDetail.js
@@ -1,2 +1,38 @@
 import React from "react";
-import { useGetUserQuery } from "../store/authApi";
+import { useGetUsersQuaery } from "../store/usersApi";
+import produceList from "../Produce/produceList";
+
+return (
+    <div class="big boi">
+        <div class="text-gray-900 font-bold text-xl mb-2">
+            <p>Name: {user.first_name + " " + user.last_name}</p>
+            <p>Loction: {user.state + ", " + user.city}</p>
+            <p>Address: {user.address}</p>
+            <p>Email: {user.email}</p>
+            <p>Phone number: {user.phone_number}</p>
+        </div>
+        <div>
+            <p> List of Produce</p>
+            <produceList></produceList>
+            {/* for each produce in produce list, create a item row
+            if item row is clicked on, expand and show buttons to
+            1)create a post for that produce
+            2) create a delivery request for that produce
+            3) delete that produce */}
+        </div>
+        <div>
+            <p> Active Delivery Requests</p>
+            {/* posts by user */}
+            <PostsList></PostsList>
+        </div>
+        <div>
+            <p> history</p>
+            {/* get all posts, filter by isCompleted ? */}
+        </div>
+
+
+
+
+
+    </div
+)
