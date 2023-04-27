ec195ac4e1910111b2be75cea509cd4dae9a5034
Garrett Hoffman <son0hoff@gmail.com>
2023-04-27 00:47:18 -0700

    flake8 and black

--- byob-service/tests/test_get_all_posts.py
+++ byob-service/tests/test_get_all_posts.py
@@ -19,22 +19,18 @@ post_mock = {
     "postimg_url": "string",
     "poster_id": 1,
     "produce": {
-      "produce_id": 1,
-      "name": "string",
-      "quantity": 10,
-      "weight": 10,
-      "description": "string",
-      "image_url": "string",
-      "exp_date": "2023-04-26",
-      "is_decorative": False,
-      "is_available": True,
-      "price": 0.0
+        "produce_id": 1,
+        "name": "string",
+        "quantity": 10,
+        "weight": 10,
+        "description": "string",
+        "image_url": "string",
+        "exp_date": "2023-04-26",
+        "is_decorative": False,
+        "is_available": True,
+        "price": 0.0,
     },
-    "user": {
-      "user_id": 1,
-      "username": "string",
-      "avatar_url": "string"
-    }
+    "user": {"user_id": 1, "username": "string", "avatar_url": "string"},
 }
 
 
@@ -53,7 +49,7 @@ mock_user = {
     "car_model": "string",
     "license_plate": "string",
     "dl_number": "string",
-    "hashed_password": "string"
+    "hashed_password": "string",
 }
 
 
@@ -62,21 +58,19 @@ def account_override():
 
 
 def test_get_all_posts_protected():
-
     app.dependency_overrides[PostsRepo] = PostsQueries
     app.dependency_overrides[
         authenticator.try_get_current_account_data
     ] = account_override
 
-    #Act
+    # Act
     response = client.get("/posts")
 
     app.dependency_overrides = {}
 
-    #Assert
+    # Assert
     assert response.status_code == 200
     assert response.json() == [post_mock]
-
 
 
 def test_init():
