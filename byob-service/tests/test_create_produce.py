5f735e3a4f9a8ee97483ec060c088f4f6509e0ec
David Jimenez <davidjimenezbzns@gmail.come>
2023-04-27 18:35:29 -0700

    this branch is a dud. Will no longer use

--- byob-service/tests/test_create_produce.py
+++ byob-service/tests/test_create_produce.py
@@ -1,16 +1,46 @@
-# from fastapi import FastAPI
+
 from fastapi.testclient import TestClient
-from fastapi import status
 from main import app
 from authenticator import authenticator
-from queries.produce import ProduceRepo
+from queries.posts import PostsRepo
 
 
 client = TestClient(app)
 
-def fake_get_current_data():
-    fake_account = {
-        "user_id": 0,
+
+class PostsQueries:
+    def get_post(self, posts_id: 2):
+        post_mock = {
+            "posts_id": 2,
+            "post_created": "2023-04-27T02:59:05.555073",
+            "text": "string",
+            "postimg_url": "string",
+            "poster_id": 2,
+            "produce": {
+                "produce_id": 2,
+                "name": "string",
+                "quantity": 20,
+                "weight": 20,
+                "description": "string",
+                "image_url": "string",
+                "exp_date": "2023-04-28",
+                "is_decorative": True,
+                "is_available": True,
+                "price": 20.22
+            },
+            "user": {
+                "user_id": 2,
+                "username": "string",
+                "avatar_url": "string"
+            }
+        }
+
+        return post_mock
+
+
+def fake_get_current_account_data():
+    return {
+        "user_id": 2,
         "first_name": "string",
         "last_name": "string",
         "email": "string",
@@ -24,46 +54,114 @@ def fake_get_current_data():
         "car_model": "string",
         "license_plate": "string",
         "dl_number": "string",
-        "hashed_password": "string"
-        }
-    return fake_account
+        "hashed_password": "$2b$12$esVcCggUoG7xmv2HogSqSuCOq/7QidUvxcAtVzuVSRYnA690O2s86",
+    }
 
-class CreateProduceRepo:
-    def create(self, produce):
-        result = {
-            "name": "string",
-            "quantity": 0,
-            "weight": 0,
-            "description": "string",
-            "image_url": "string",
-            "exp_date": "2023-04-25",
-            "is_decorative": True,
-            "is_available": True,
-            "price": 0,
-            "owner_id": 0
-        }
-        result.update(produce)
 
-        return produce
 
-def test_create_produce():
-    app.dependency_overrides[ProduceRepo] = CreateProduceRepo
+def test_get_post_protected():
+    app.dependency_overrides[PostsRepo] = PostsQueries
     app.dependency_overrides[
-        authenticator.try_get_current_data
-    ] = fake_get_current_data
+        authenticator.try_get_current_account_data
+    ] = fake_get_current_account_data
 
-    response = client.post("/user/${user_id}/produce", json={
-        "name": "carrot",
-        "quantity": "12",
-        "weight": "10lbs",
-        "description": "orange,pointy,good for rabbits",
-        "image_url": "blank",
-        "exp_date": "2025-10-12",
-        "is_decorative": True,
-        "is_available": True,
-        "price": "10",
-    })
+    response = client.get("/posts/2")
 
     app.dependency_overrides = {}
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {"message": "Produce Creation Success"}
+
+    # Assert
+    assert response.status_code == 200
+    assert response.json() == {
+        "posts_id": 2,
+        "post_created": "2023-04-27T02:59:05.555073",
+        "text": "string",
+        "postimg_url": "string",
+        "poster_id": 2,
+        "produce": {
+            "produce_id": 2,
+            "name": "string",
+            "quantity": 20,
+            "weight": 20,
+            "description": "string",
+            "image_url": "string",
+            "exp_date": "2023-04-28",
+            "is_decorative": True,
+            "is_available": True,
+            "price": 20.22
+        },
+        "user": {
+            "user_id": 2,
+            "username": "string",
+            "avatar_url": "string"
+        }
+    }
+
+# def test_init():
+#     assert 1 == 1
+
+# def fake_get_current_data():
+#     fake_account = {
+#         "user_id": 1,
+#         "first_name": "string",
+#         "last_name": "string",
+#         "email": "string",
+#         "phone_number": "string",
+#         "address": "string",
+#         "city": "string",
+#         "state": "string",
+#         "username": "string",
+#         "avatar_url": "string",
+#         "is_driver": True,
+#         "car_model": "string",
+#         "license_plate": "string",
+#         "dl_number": "string",
+#         "hashed_password": "string"
+#     }
+#     return fake_account
+
+
+# class CreateProduceRepo:
+#     def create(self, produce):
+#         result = {
+#             "name": "string",
+#             "quantity": 0,
+#             "weight": 0,
+#             "description": "string",
+#             "image_url": "string",
+#             "exp_date": "2023-04-28",
+#             "is_decorative": True,
+#             "is_available": True,
+#             "price": 0.0,
+#             "owner_id": 0
+#         }
+#         result.update(produce)
+
+#         return result
+
+
+# def test_create_produce():
+#     # Arrange
+#     app.dependency_overrides[ProduceRepo] = CreateProduceRepo
+#     app.dependency_overrides[
+#         authenticator.try_get_current_account_data
+#     ] = fake_get_current_data
+
+#     # Act
+#     json = {
+#         "name": "string",
+#         "quantity": 0,
+#         "weight": 0,
+#         "description": "string",
+#         "image_url": "string",
+#         "exp_date": "2023-04-28",
+#         "is_decorative": True,
+#         "is_available": True,
+#         "price": 0.0,
+#         "user_id" : 0,
+#     }
+#     response = client.post("/users/${user_id}/produce", json=json)
+
+#     app.dependency_overrides = {}
+#     # Assert
+#     assert response.status_code == 200
+#     assert response.json() == {"message": "Produce Creation Success"}
